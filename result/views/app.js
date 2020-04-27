f7fb568ec8189f1be5b61863c6fde9d3b3011949
阿部翼 <t-abe@ttc-net.co.jp>
2020-04-27 10:08:59 +0900

    AngularJSからVue.jsに変更

--- result/views/app.js
+++ result/views/app.js
@@ -1,50 +1,53 @@
-var app = angular.module('catsvsdogs', []);
-var socket = io.connect({transports:['polling']});
+const socket = io.connect({transports:['polling']});
 
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
+new Vue({
+  el: "#app",
 
-app.controller('statsCtrl', function($scope){
-  $scope.aPercent = 50;
-  $scope.bPercent = 50;
+  data: {
+    aCategory: "50%",
+    bCategory: "50%",
+    total: 0,
+  },
 
-  var updateScores = function(){
-    socket.on('scores', function (json) {
-       data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+  methods: {
+    getPercentages: function (a, b) {
+      let result = {};
+    
+      if (a + b > 0) {
+        result.a = Math.round(a / (a + b) * 100);
+        result.b = 100 - result.a;
+      } else {
+        result.a = result.b = 50;
+      }
+    
+      return result;
+    },
 
-       var percentages = getPercentages(a, b);
+    updateScores: function() {
+      const that = this;
+      socket.on('scores', function (json) {
+        data = JSON.parse(json);
+        const a = parseInt(data.a || 0);
+        const b = parseInt(data.b || 0);
+  
+        const percentages = that.getPercentages(a, b);
+  
+        that.aCategory = percentages.a + "%";
+        that.bCategory = percentages.b + "%";
+        that.total = a + b;
+     });
+    },
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
+    init: function() {
+      document.body.style.opacity = 1;
+      this.updateScores();
+    },
+  },
 
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
+  created: function () {
+    const that = this;
+    socket.on('message',function(data){
+      that.init();
     });
-  };
-
-  var init = function(){
-    document.body.style.opacity=1;
-    updateScores();
-  };
-  socket.on('message',function(data){
-    init();
-  });
-});
-
-function getPercentages(a, b) {
-  var result = {};
-
-  if (a + b > 0) {
-    result.a = Math.round(a / (a + b) * 100);
-    result.b = 100 - result.a;
-  } else {
-    result.a = result.b = 50;
   }
-
-  return result;
-}
+});
